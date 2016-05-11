e67f3ce807cd0b556ca2a48084d758dc0b0ec5f4
Kevin Harris <kharris1@fossil.com>
2016-05-11 09:26:31 -0500

    Cleanup switch statements so it will fit on the screen better.

--- java_5_conditional_logic/src/org/guildsa/Main.java
+++ java_5_conditional_logic/src/org/guildsa/Main.java
@@ -60,44 +60,20 @@ public class Main {
 
         switch(monthNumber) {
 
-            case 1:  monthString = "January";
-                break;
+            case 1: monthString = "January"; break;
+            case 2: monthString = "February"; break;
+            case 3: monthString = "March"; break;
+            case 4: monthString = "April"; break;
+            case 5: monthString = "May"; break;
+            case 6: monthString = "June"; break;
+            case 7: monthString = "July"; break;
+            case 8: monthString = "August"; break;
+            case 9: monthString = "September"; break;
+            case 10: monthString = "October"; break;
+            case 11: monthString = "November"; break;
+            case 12: monthString = "December"; break;
 
-            case 2:  monthString = "February";
-                break;
-
-            case 3:  monthString = "March";
-                break;
-
-            case 4:  monthString = "April";
-                break;
-
-            case 5:  monthString = "May";
-                break;
-
-            case 6:  monthString = "June";
-                break;
-
-            case 7:  monthString = "July";
-                break;
-
-            case 8:  monthString = "August";
-                break;
-
-            case 9:  monthString = "September";
-                break;
-
-            case 10: monthString = "October";
-                break;
-
-            case 11: monthString = "November";
-                break;
-
-            case 12: monthString = "December";
-                break;
-
-            default: monthString = "Invalid month passed!";
-                break;
+            default: monthString = "Invalid month passed!"; break;
         }
 
         System.out.println("monthString = " + monthString);
@@ -111,57 +87,20 @@ public class Main {
 
         switch(monthName) {
 
-            case "january":
-                monthNumber = 1;
-                break;
+            case "january": monthNumber = 1; break;
+            case "february": monthNumber = 2; break;
+            case "march": monthNumber = 3; break;
+            case "april": monthNumber = 4; break;
+            case "may": monthNumber = 5; break;
+            case "june": monthNumber = 6; break;
+            case "july": monthNumber = 7; break;
+            case "august": monthNumber = 8; break;
+            case "september": monthNumber = 9; break;
+            case "october": monthNumber = 10; break;
+            case "november": monthNumber = 11; break;
+            case "december": monthNumber = 12; break;
 
-            case "february":
-                monthNumber = 2;
-                break;
-
-            case "march":
-                monthNumber = 3;
-                break;
-
-            case "april":
-                monthNumber = 4;
-                break;
-
-            case "may":
-                monthNumber = 5;
-                break;
-
-            case "june":
-                monthNumber = 6;
-                break;
-
-            case "july":
-                monthNumber = 7;
-                break;
-
-            case "august":
-                monthNumber = 8;
-                break;
-
-            case "september":
-                monthNumber = 9;
-                break;
-
-            case "october":
-                monthNumber = 10;
-                break;
-
-            case "november":
-                monthNumber = 11;
-                break;
-
-            case "december":
-                monthNumber = 12;
-                break;
-
-            default:
-                monthNumber = 0;
-                break;
+            default: monthNumber = 0; break;
         }
 
         System.out.println("monthNumber = " + String.valueOf(monthNumber));
